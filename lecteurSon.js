9d97c98408a72cc64b6c8fec0937fb4ca27b5de2
Marc Fortier <marc.fortier.1@gmail.com>
2019-03-05 12:09:51 -0500

    Update lecteurSon.js

--- lecteurSon.js
+++ lecteurSon.js
@@ -1,5 +1,5 @@
 var son = new Audio('son.wav'); // par défaut
-
+var versionIncrement = 0; // V2 : obligé de faire ça pour recharger un son ...
 var infoTemps = document.getElementById("div-infos-temps");
 var inpTemps = document.getElementById("inp-temps");
 var temps = 0;
@@ -21,6 +21,7 @@ son.addEventListener('ended', sonEstFini);
 son.addEventListener('pause', sonEstFini);
 son.addEventListener('play', sonJoue);
 son.addEventListener('canplaythrough', sonEstCharge); // chargement
+son.addEventListener('canplaythrough', sonEstFini); // Ajouté pour mettre l'état pause après un rechargement
 son.addEventListener('error', erreurChargement); // chargement
 
 function analyseTempsCompens(e) {
@@ -57,13 +58,18 @@ function analyseTemps(e) {
 		temps = tempsArray[0]*60 + tempsArray[1];
 	} else if (tempsArray.length == 3) {
 		
-		var milli = tempsArray[2];
+		// remplacer cette ligne par le commentaire ci-dessous
+		// pour avoir le dernier nombre en ms
+		// Changer 30 par 25 pour changer le framerate
+		var milli = tempsArray[2]*1000/30;
+		
+		/*var milli = tempsArray[2];
 	
 		if(milli >= 1000) milli = 0;
 		else if(milli>=100 && milli<1000) milli = milli;
 		else if(milli>=10 && milli<100) milli *= 10;
 		else if(milli>=0 && milli<10) milli *= 100;
-		else milli = 0;
+		else milli = 0;*/
 
 		temps = tempsArray[0]*60 + tempsArray[1] + milli/1000;
 	}
@@ -73,7 +79,8 @@ function analyseTemps(e) {
 }
 
 function sonEstFini(e) {
-	document.getElementById("div-statut").innerHTML = "<font color='red'>Statut : à l'arrêt";
+	if(son.paused)
+		document.getElementById("div-statut").innerHTML = "<font color='red'>Statut : à l'arrêt";
 }
 
 function sonJoue(e) {			
@@ -104,5 +111,11 @@ function peseSpace(e) {
 	} else if (e.key == 'Control') {
 		inpTemps.value = ''
 		inpTemps.focus();
+	} else if (e.key == 'r') {  // Ajouté à V2, pour recharger même fichier
+		// ajoute une version, sinon le fichier n'est pas chargé
+		// Note : tous les fichiers restent en cache!! donc au bout d'un certain temps,
+		//        il y aura une perte de performance (plein de RAM utilisée par le browser...
+		++versionIncrement;
+		son.src = document.getElementById("inp-fichier").value + '?' + versionIncrement;
 	}
 }
